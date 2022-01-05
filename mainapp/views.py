f452ef8208dd33e83e6bbc968b2ce59698d2b68c
Vadim <joattt@gmail.com>
2022-01-06 01:53:22 +1000

    add lesson_8

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
 from mainapp.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def get_basket(user):
@@ -25,38 +26,47 @@ def get_same_products(hot_product):
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'каталог'
     links_menu = ProductCategory.objects.all()
     basket = get_basket(request.user)
-    hot_proudct = get_hot_product()
-    same_products = get_same_products(hot_proudct)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     products = Product.objects.all().order_by('price')
 
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
-            category = {'name': 'все'}
+            category = {'pk': 0, 'name': 'все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
 
+        paginator = Paginator(products, 1)
+
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
         context = {
             'title': title,
             'links_menu': links_menu,
-            'products': products,
+            'products': products_paginator,
             'category': category,
             'basket': basket,
-            'hot_product': hot_proudct,
+            'hot_product': hot_product,
             'same_products': same_products,
-    }
+        }
         return render(request, 'mainapp/products.html', context=context)
 
     context = {
         'title': title,
         'links_menu': links_menu,
         'products': products,
-        'hot_product': hot_proudct,
+        'hot_product': hot_product,
         'same_products': same_products,
         'basket': basket,
     }
